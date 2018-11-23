da6aa8d3f3c046e9bb2d377d4d7e66df83dff7a2
Mike Mostovyi <mike@ngti.nl>
2018-11-23 19:05:25 +0100

    Showing the foreground notification

--- utils-debug/src/main/java/nl/ngti/debugwebserver/DebugWebServerActivity.java
+++ utils-debug/src/main/java/nl/ngti/debugwebserver/DebugWebServerActivity.java
@@ -1,15 +1,12 @@
 package nl.ngti.debugwebserver;
 
 import android.app.Activity;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 
 public class DebugWebServerActivity extends Activity {
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onResume() {
+        super.onResume();
         DebugWebServerStartService.startServer(this, true);
-        moveTaskToBack(true);
     }
 }
--- utils-debug/src/main/java/nl/ngti/debugwebserver/DebugWebServerStartService.java
+++ utils-debug/src/main/java/nl/ngti/debugwebserver/DebugWebServerStartService.java
@@ -3,6 +3,7 @@ package nl.ngti.debugwebserver;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -27,9 +28,10 @@ public class DebugWebServerStartService extends Service {
     private static final String TAG = DebugWebServerStartService.class.getSimpleName();
 
     private static final String ACTION_START_WEB_SERVER = "nl.ngti.debugwebserver.action.START_WEB_SERVER";
+    private static final String ACTION_STOP_WEB_SERVER = "nl.ngti.debugwebserver.action.STOP_WEB_SERVER";
     private static final String KEY_FORCE = "nl.ngti.debugwebserver.key.FORCE";
 
-    private static boolean sServerStarted;
+    private NanoHTTPD mServer;
 
     public static void startServer(Context context, boolean force) {
         Intent intent = new Intent(context, DebugWebServerStartService.class);
@@ -42,33 +44,72 @@ public class DebugWebServerStartService extends Service {
     public int onStartCommand(Intent intent, int flags, int startId) {
         if (intent != null) {
             final String action = intent.getAction();
-            if (ACTION_START_WEB_SERVER.equals(action)) {
-                boolean force = intent.getBooleanExtra(KEY_FORCE, false);
-                handleServerStart(force);
+            if (action != null) {
+                switch (action) {
+                    case ACTION_START_WEB_SERVER:
+                        boolean force = intent.getBooleanExtra(KEY_FORCE, false);
+                        handleServerStart(force);
+                        break;
+                    case ACTION_STOP_WEB_SERVER:
+                        handleServerStop();
+                        break;
+                }
             }
         }
         return START_NOT_STICKY;
     }
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        stopServer();
+    }
+
+    private boolean stopServer() {
+        if (mServer != null) {
+            mServer.stop();
+            mServer = null;
+            return true;
+        }
+        return false;
+    }
+
     private void handleServerStart(boolean force) {
-        if (sServerStarted) {
+        if (mServer != null && mServer.isAlive()) {
             showToast("Debug Web Server is already running");
             return;
         }
 
-        NanoHTTPD server = DeviceExplorerHttpServer.newInstance(this, force);
-        if (server != null) {
+        mServer = DeviceExplorerHttpServer.newInstance(this, force);
+        if (mServer != null) {
             showNotification();
 
-            IoServerRunner.executeInstance(this, server, new NanoListener());
-            sServerStarted = true;
+            IoServerRunner.executeInstance(this, mServer, new NanoListener());
             showToast("Debug Web Server was started");
         }
     }
 
+    private void handleServerStop() {
+        if (stopServer()) {
+            stopForeground(true);
+        }
+    }
+
     private void showNotification() {
+        final Intent intent = new Intent(this, DebugWebServerStartService.class);
+        intent.setAction(ACTION_STOP_WEB_SERVER);
+
+        final PendingIntent pendingIntent = PendingIntent.getService(this,
+                R.id.androscope_notification_request_code_stop_server, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        final NotificationCompat.Action action = new NotificationCompat.Action.Builder(0, getString(R.string.androscope_stop_server), pendingIntent)
+                .build();
+
         final Notification notification = getNotificationBuilder()
+                .setSmallIcon(R.drawable.androscope_notification_icon)
                 .setContentText("Androscope is running")
+                .addAction(action)
                 .build();
 
         startForeground(R.id.androscope_notification_id, notification);
