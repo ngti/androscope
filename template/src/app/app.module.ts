7f06b80edf80b4cc5741dd23f3b4817ca415cd9f
Mike Mostovyi <mike@ngti.nl>
2019-12-31 13:37:28 +0100

    Added toolbar and footer

--- template/src/app/app.module.ts
+++ template/src/app/app.module.ts
@@ -7,15 +7,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
-import {MatButtonModule, MatFormFieldModule, MatInputModule} from '@angular/material';
+import {
+  MatButtonModule, MatDividerModule,
+  MatFormFieldModule, MatIconModule,
+  MatInputModule, MatMenuModule,
+  MatToolbarModule
+} from '@angular/material';
 import { QueryContentProviderComponent } from './query-content-provider/query-content-provider.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import { ToolbarComponent } from './navigation/toolbar/toolbar.component';
+import { FooterComponent } from './navigation/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QueryContentProviderComponent
+    QueryContentProviderComponent,
+    ToolbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
@@ -28,7 +37,11 @@ import {HttpClientModule} from '@angular/common/http';
     MatFormFieldModule,
     MatInputModule,
     FormsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatMenuModule,
+    MatDividerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ template/src/app/navigation/toolbar/toolbar.component.ts
@@ -0,0 +1,11 @@
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component, OnDestroy} from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.css']
+})
+export class ToolbarComponent {
+
+}
