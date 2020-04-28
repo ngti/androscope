8975f0537dbdc8a3ff1f9ac63f8ecf6da91f941a
Mike Mostovyi <mike@ngti.nl>
2020-04-28 09:21:55 +0200

    Database info menu cleanup

--- template/src/app/database/database-info/database-info.component.ts
+++ template/src/app/database/database-info/database-info.component.ts
@@ -1,42 +1,20 @@
-import {Component, Input, OnInit, ViewChild} from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {DatabaseInfo} from '../../common/rest/database-data';
-import {log} from "util";
-import {MatMenuTrigger} from "@angular/material/menu";
 
 @Component({
   selector: 'app-database-info',
   templateUrl: './database-info.component.html',
   styleUrls: ['./database-info.component.css']
 })
-export class DatabaseInfoComponent implements OnInit {
+export class DatabaseInfoComponent {
 
   @Input('data')
   databaseInfo: DatabaseInfo;
 
-  @ViewChild(MatMenuTrigger, {static: false}) trigger: MatMenuTrigger;
-
   constructor() { }
-
-  ngOnInit() {
-  }
 
   onMenuButtonClick($event: MouseEvent) {
     $event.preventDefault();
     $event.stopImmediatePropagation();
-  }
-
-  onTableMouseUp($event: MouseEvent) {
-    log('onTableMouseUp ' + $event.button);
-  }
-
-  onTableClick($event: MouseEvent) {
-    log('onTableClick');
-  }
-
-  onTableMouseDown($event: MouseEvent) {
-    log('onTableMouseDown ' + $event.button);
-    if ($event.button === 2) {
-      this.trigger.openMenu();
-    }
   }
 }
