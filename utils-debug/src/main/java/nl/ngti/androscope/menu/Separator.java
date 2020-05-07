d421e096be29fa96b2d2a590aea05b8bd0c8e349
Mike Mostovyi <mike@ngti.nl>
2020-05-07 18:34:24 +0200

    Removed old Androscope implementation

--- utils-debug/src/main/java/nl/ngti/androscope/menu/Separator.java
+++
@@ -1,9 +0,0 @@
-package nl.ngti.androscope.menu;
-
-final class Separator implements IMenuItem {
-
-    @Override
-    public void render(StringBuilder html) {
-        html.append("<p/>");
-    }
-}
