87e8512c5172dc5763112014f1fbdedddd07c3d4
Mike Mostovyi <mike@ngti.nl>
2020-05-13 17:01:45 +0200

    Better notification compatibility

--- lib-androscope/src/main/java/nl/ngti/androscope/service/AndroscopeNotificationHelper.kt
+++ lib-androscope/src/main/java/nl/ngti/androscope/service/AndroscopeNotificationHelper.kt
@@ -4,12 +4,12 @@ import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.app.Service
-import android.content.Context
 import android.content.Intent
 import android.os.Build
 import android.os.Handler
 import android.os.SystemClock
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import nl.ngti.androscope.AndroscopeActivity
 import nl.ngti.androscope.R
 
@@ -17,23 +17,33 @@ internal class AndroscopeNotificationHelper(
         private val service: Service,
         private val handler: Handler
 ) {
-
-    private val notificationManager: NotificationManager
-        get() = service.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
     private val showPendingNotification = Runnable {
         pendingNotification?.run {
             showNotificationNow(this)
         }
     }
 
+    private val channelId by lazy {
+        service.getString(R.string.androscope_channel_id).also {
+            maybeCreateChannel(it)
+        }
+    }
+
+    private val currentTime get() = SystemClock.elapsedRealtime()
+
     private var lastNotificationShowTime: Long = 0
     private var pendingNotification: NotificationCompat.Builder? = null
 
     fun showNotification(setupBlock: NotificationCompat.Builder.() -> Unit) {
-        val notificationBuilder = newNotificationBuilder()
+        val notificationBuilder = NotificationCompat.Builder(service, channelId)
         setupBlock(notificationBuilder)
         notificationBuilder.run {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+                // Android before Nougat does not display application name in notifications.
+                // So we need to manually set it in the notification title.
+                val applicationName = service.packageManager.getApplicationLabel(service.applicationInfo)
+                setContentTitle(applicationName)
+            }
             setSmallIcon(R.drawable.androscope_notification_icon)
             setContentIntent(PendingIntent.getActivity(service,
                     R.id.androscope_notification_request_code_open_androscope_activity,
@@ -42,19 +52,6 @@ internal class AndroscopeNotificationHelper(
 
         handler.post {
             queueNotification(notificationBuilder)
-        }
-    }
-
-    private fun newNotificationBuilder(): NotificationCompat.Builder {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channelId = service.getString(R.string.androscope_channel_id)
-            val channelName = service.getString(R.string.androscope_channel_name)
-            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_MIN)
-            notificationManager.createNotificationChannel(channel)
-            NotificationCompat.Builder(service, channelId)
-        } else {
-            @Suppress("DEPRECATION")
-            NotificationCompat.Builder(service)
         }
     }
 
@@ -74,7 +71,15 @@ internal class AndroscopeNotificationHelper(
         lastNotificationShowTime = currentTime
     }
 
-    private val currentTime get() = SystemClock.elapsedRealtime()
+    private fun maybeCreateChannel(channelId: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelName = service.getString(R.string.androscope_channel_name)
+            NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_MIN).also {
+                val notificationManager = NotificationManagerCompat.from(service)
+                notificationManager.createNotificationChannel(it)
+            }
+        }
+    }
 
     companion object {
         /**
